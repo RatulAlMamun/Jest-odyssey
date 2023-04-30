f1b7b9cae685e56a1d9b5cd91bcdc49d7d70241b
RatulAlMamun <ratulalmamun23@gmail.com>
2023-04-30 13:22:47 +0600

    install dotenv package.

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
 
 const app = express();
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
